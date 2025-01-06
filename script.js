42812fca98d9c3838f429fb69b048eba66fea26e
aganaber111 <ardaofarda81@gmail.com>
2025-01-06 08:52:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// Koyu mod açma/kapama butonu için öğeyi seç
 const toggleButton = document.getElementById('toggleButton');
-
-// Body elementini al
 const body = document.body;
 
-// Butona tıklama olayını dinleyin
 toggleButton.addEventListener('click', () => {
-    // Body etiketine 'dark-mode' sınıfını ekle veya kaldır
     body.classList.toggle('dark-mode');
 });
